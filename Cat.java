ecb4103a01b52a0dc94030a4b8241264be7f4fc6
Md Rakibul Islam <55613010+rakibgiant@users.noreply.github.com>
2020-08-03 00:12:28 +0600

    add print

--- Cat.java
+++ Cat.java
@@ -31,4 +31,9 @@ public class Cat extends Pet {
   public void setLook(String look) {
     this.look = look;
   }
+  @Override
+  public void print() {
+    super.print();
+    System.out.println("I am a " + this.look);
+  }  
 }
